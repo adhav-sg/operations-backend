c818f2ed84b3cf0ce92eb96f790f9faee3140365
Adhavan SG <adhavansg@Adhavans-MacBook-Air.local>
2025-03-06 10:12:35 +0530

    feat: add dotenv for environment variable management

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+// filepath: [app.module.ts](http://_vscodecontentref_/0)
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -5,11 +6,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ContractSchema } from './schema/contract.schema';
 import { ContractManagementService } from './contractManagement/contractManagement.service';
 import { ContractManagementController } from './contractManagement/contractManagement.controller';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
     MongooseModule.forRoot(
-      
+      process.env.MONGODB_URI || 'mongodb://localhost:27017/defaultdb',
     ),
     MongooseModule.forFeature([{ name: 'Contract', schema: ContractSchema }]),
   ],
